5aa8a32e55e8f91213c290d26fddcffd91b9eb07
Hoang Xuan Thanh <thanh.hx@zinza.com.vn>
2017-10-09 17:44:54 +0700

    tenten

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = @q.result.includes(:tags).order(created_at: :desc).page(params[:page])
--- app/controllers/translated_posts_controller.rb
+++ app/controllers/translated_posts_controller.rb
@@ -1,5 +1,5 @@
 class TranslatedPostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   def index
   end
 
